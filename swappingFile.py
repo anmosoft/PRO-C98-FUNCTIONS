f8cd2f4553fb1e1028fab8956aabc96c57a577de
anmosoft <72310713+anmosoft@users.noreply.github.com>
2022-07-09 14:20:18 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,14 @@
+def swapFiledata():
+    file1=input("enter file 1 'type text1.txt':")
+    file2=input("enter file 2 'type text2.txt':")
+    with open(file1,'r')as a:
+        data_a=a.read()
+    with open(file2,'r')as b:
+        data_b=b.read()
+    with open(file1,'w')as a:
+        a.write(data_b)
+    with open(file2,'w')as b:
+        b.write(data_a)
+
+
+swapFiledata()
